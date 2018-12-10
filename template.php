8cdac7cdc360b487b5582e52e0b2aa47b0a969d4
EBoblett949 <31224633+EBoblett949@users.noreply.github.com>
2018-12-10 13:13:27 -0700

    Add files via upload

---
+++ template.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <!-- 
+        Exercise xx_xx_xx
+        Author: Eli Boblett
+        Date: xx.xx.18 
+        .php
+     -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <script src="modernizr.custom.65897.js"></script>
+</head>
+<body>
+    <?php
+    
+    ?>
+</body>
+</html>
